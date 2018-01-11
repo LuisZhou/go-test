5ef29837f1a182c8b2a4cce628b93318ba2e16e1
luiszhou <luis_zhou@twinstargame.com>
2018-01-11 23:05:28 +0800

    update

---
+++ little-book-of-golang/src/chapter6/main/select-timeout.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Worker struct {
+	id int
+}
+
+func main() {
+	c := make(chan int)
+	for i := 0; i < 1; i++ {
+		worker := &Worker{id: i}
+		go worker.process(c)
+	}
+
+	// The first available channel is chosen.
+	// If multiple channels are available, one is randomly picked.
+	// If no channel is available, the default case is executed.
+	// If there's no default, select blocks.
+
+	i := 1
+	for {
+		select {
+		case c <- i: // block to wait it
+		case <-time.After(time.Millisecond * 50): // block to wait it
+			//fmt.Println("timed out ")
+			fmt.Printf("timed out %d\n", i)
+		}
+		time.Sleep(time.Millisecond * 50)
+		i++
+	}
+}
+
+func (w Worker) process(c chan int) {
+	for {
+		data := <-c
+		// fmt.Println(len(c))
+		fmt.Printf("worker %d got %d\n", w.id, data)
+		time.Sleep(time.Millisecond * 100)
+	}
+}

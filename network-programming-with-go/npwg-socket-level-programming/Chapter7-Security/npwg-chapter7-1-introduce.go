2ef77b288805593eada3448ff3f694c84798b90d
wangqian <luis.zhou.zw@gamil.com>
2018-03-07 19:03:56 +0800

    update

---
+++ network-programming-with-go/npwg-socket-level-programming/Chapter7-Security/npwg-chapter7-1-introduce.go
@@ -0,0 +1,3 @@
+// Applications have to be built to work correctly in hostile situations. "correctly" no longer means just getting the 
+// functional aspects of the program correct, but also means ensuring privacy and integrity of data transferred, access 
+// only to legitimate users and other issues.
---
+++ network-programming-with-go/npwg-socket-level-programming/Chapter7-Security/npwg-chapter7-2-ISO-security-architecture.go
@@ -0,0 +1,9 @@
+// # ISO Security Architecture model, ISO 7498-2
+
+// ## Functions and levels
+// + Authentication - proof of identity
+// + Data integrity - data is not tampered with
+// + Confidentiality - data is not exposed to others
+// + Notarization/signature
+// + Access control
+// + Assurance/availability

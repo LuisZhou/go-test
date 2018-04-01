5cf260e281eeceb8db3119210d41e0ed87b3d9e8
luiszhou <luis_zhou@twinstargame.com>
2018-04-01 23:10:46 +0800

    update

---
+++ network-programming-with-go/npwg-socket-level-programming/Chapter8-Overview-of-HTTP/npwg-chapter8-1-URLs-and-resources.go
@@ -0,0 +1,14 @@
+package main
+
+// URLs specify the location of a resource
+
+// resource:
+// HTML document, an image, or a sound file, dynamically generated object
+
+// really?
+// When a user agent requests a resource, what is returned is not the resource itself, but some representation of that
+// resource. For example, if the resource is a static file, then what is sent to the user agent is a __copy__ of the file.
+
+func main() {
+
+}

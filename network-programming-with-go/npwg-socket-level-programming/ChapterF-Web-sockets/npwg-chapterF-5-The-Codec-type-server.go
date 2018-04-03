29096d18f2d4303f0c13cc92568262bce16a160d
wangqian <luis.zhou.zw@gamil.com>
2018-04-03 18:15:21 +0800

    update

---
+++ network-programming-with-go/npwg-socket-level-programming/ChapterF-Web-sockets/npwg-chapterF-5-The-Codec-type-server.go
@@ -0,0 +1,53 @@
+/* PersonServerXML
+ */
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/websocket"
+	"net/http"
+	"os"
+	"xmlcodec"
+)
+
+type Person struct {
+	Name   string
+	Emails []string
+}
+
+// Message and JSON objects are both instances of the type Codec:
+
+// careful: not a interface.
+
+// type Codec struct {
+// 	Marshal   func(v interface{}) (data []byte, payloadType byte, err error)
+// 	Unmarshal func(data []byte, payloadType byte, v interface{}) (err error)
+// }
+
+// The type  Codec  implements the  Send  and  Receive  methods used earlier.
+
+func ReceivePerson(ws *websocket.Conn) {
+	var person Person
+	err := xmlcodec.XMLCodec.Receive(ws, &person)
+	if err != nil {
+		fmt.Println("Can't receive")
+	} else {
+		fmt.Println("Name: " + person.Name)
+		for _, e := range person.Emails {
+			fmt.Println("An email: " + e)
+		}
+	}
+}
+
+func main() {
+	http.Handle("/", websocket.Handler(ReceivePerson))
+	err := http.ListenAndServe(":12345", nil)
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
--- network-programming-with-go/npwg-socket-level-programming/ChapterF-Web-sockets/npwg-chapterF-6-Web-sockets-over-TLS-client.go
+++ network-programming-with-go/npwg-socket-level-programming/ChapterF-Web-sockets/npwg-chapterF-6-Web-sockets-over-TLS-client.go
@@ -3,10 +3,16 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"golang.org/x/net/websocket"
 	"io"
+	"log"
+	"net"
+	_ "net/http"
+	"net/http/httptest"
 	"os"
+	"time"
 )
 
 func main() {
@@ -16,8 +22,23 @@ func main() {
 	}
 	service := os.Args[1]
 
+	tlsServer := httptest.NewTLSServer(nil)
+	tlsServerAddr := tlsServer.Listener.Addr().String()
+	log.Print("Test TLS WebSocket server listening on ", tlsServerAddr)
+	defer tlsServer.Close()
+	// config, _ := NewConfig(fmt.Sprintf("wss://%s/echo", tlsServerAddr), "http://localhost")
+	config, _ := websocket.NewConfig(service, "http://localhost")
+
+	config.Dialer = &net.Dialer{
+		Deadline: time.Now().Add(time.Minute * 30), // if use -time.Minute will get 'dial tcp: i/o timeout'
+	}
+	config.TlsConfig = &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	conn, err := websocket.DialConfig(config)
+
 	// config := tls.Config{RootCAs: nil, InsecureSkipVerify: true}
-	conn, err := websocket.Dial(service, "", "http://localhost")
+	// conn, err := websocket.Dial(service, "", "http://localhost")
 	checkError(err)
 	var msg string
 	for {

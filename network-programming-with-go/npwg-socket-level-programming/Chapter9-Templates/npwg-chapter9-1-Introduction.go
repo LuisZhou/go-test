341a7f917902a4380ee98049ec4ff0923a92f413
luiszhou <luis.zhou.zw@gamil.com>
2018-04-08 19:13:04 +0800

    update

---
+++ network-programming-with-go/npwg-socket-level-programming/Chapter9-Templates/npwg-chapter9-1-Introduction.go
@@ -0,0 +1,20 @@
+package main
+
+// Go has a template mechanism to convert strings based on the content of an object supplied as an argument. While this
+// is often used in rewriting HTML to insert object values, it can be used in other situations.
+
+// taking predominantly static pages and inserting a dynamically generated component
+
+// Go has adopted a relatively simple scripting language in the  template package.
+
+// The package is designed to take text as input and output different text, based on transforming the original text
+// using the values of an object.
+
+// The original source is called a template and will consist of text that is transmitted unchanged,
+// and __embedded commands__ which can act on and change text.
+
+// The commands are delimited by  {{ ... }}, like PHPs  <?php ... ?>
+
+func main() {
+
+}
